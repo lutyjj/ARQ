8e88f175cac90d74f7f962e7a0ff707eb2d13df9
Daniil Rylski <addanik@gmail.com>
2020-04-21 21:14:58 +0300

    OOP-ify

--- Receiver.py
+++ Receiver.py
@@ -1,5 +1,53 @@
-# stub
+import random
+import numpy as np
+from PIL import Image
 
 class Receiver:
+    probability = 0.05
+    broken_frames = []
+    result = []
+    shape = None
+
     def __init__(self):
         pass
+
+
+    # interference
+    def interfere_array(self, array):
+        # probability of interference
+        if random.random() < self.probability:
+            # don't interfere parity bit
+            index = random.randint(0, array.size - 2)
+            array[index] = random.randint(0, 255)
+        return array
+
+
+    # sending
+    def receive_frame(self, array, index):
+        array = self.interfere_array(array)
+
+        frame_sum = 0
+        # generate parity bit
+        for i in range(len(array) - 1):
+            frame_sum += array[i]
+        received_bit = frame_sum % 2
+
+        # check for parity bit to be the same
+        # with one stored in frame as last item
+        if received_bit == array[len(array) - 1]:
+            print("Frame ", index, " is good, continuing")
+            # delete last item if bits match
+            received_frame = np.delete(array, len(array) - 1)
+            # accept received frame
+            self.result.append(received_frame)
+            return True
+        else:
+            print("Frame ", index, " is broken, repeating transmission")
+            # store index of broken frame
+            self.broken_frames.append(index)
+            return False
+
+    def finalize_img(self):
+        # reshape final array to its original shape
+        final_img = np.reshape(self.result, self.shape).astype(np.uint8)
+        return Image.fromarray(final_img)
--- Sender.py
+++ Sender.py
@@ -1,5 +1,52 @@
-# stub
+import numpy as np
 
 class Sender:
-    def __init__(self):
-        pass
+    data = []
+    shape = None
+
+    def __init__(self, receiver):
+        self.receiver = receiver
+
+
+    def parity_bit(self, array):
+        frame_sum = 0
+        for i in range(0, array.size):
+            frame_sum += array[i]
+        return frame_sum % 2
+
+
+    def split_array(self, array):
+        # store shape of original array
+        self.shape = array.shape
+        # temporary array to store splitted parts
+        tmp_arr = []
+        for i in range(len(array)):
+            arr = array[i]
+            for j in range(len(arr)):
+                # for each array of individual pixels
+                pix_arr = arr[j]
+                # generate parity bit
+                bit = self.parity_bit(pix_arr)
+                # temporary array to append parity bit
+                new_pix_arr = np.append(pix_arr, bit)
+                # append array to final array
+                tmp_arr.append(new_pix_arr)
+
+        self.data = tmp_arr
+        return tmp_arr
+
+
+    def send_frames(self):
+        # send original shape
+        self.receiver.shape = self.shape
+
+        # Stop-and-wait ARQ
+        i = 0
+        while i < len(self.data):
+            # Receive ACK
+            ACK = self.receiver.receive_frame(self.data[i], i)
+            # if ACK is good - proceed to next frame
+            # otherwise - repeat transmission
+            if ACK:
+                i += 1
+
