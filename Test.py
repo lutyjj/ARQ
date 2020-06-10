ded99d92c558cb0b22399aa1fedfaf722311b6ab
unknown <adrian.malecki99@o2.pl>
2020-06-10 02:00:25 +0200

    Added Gilbert model tests

--- Test.py
+++ Test.py
@@ -5,12 +5,13 @@ from Receiver import Receiver
 
 file = open("test_results.txt", "w")
 
-def start_test(chosen_algorithm, control_method, probability, windows_size, packet_size):
+
+def start_test(chosen_algorithm, control_method, probability, windows_size, packet_size, model, P01, P10):
     img = Image.open("test.jpg")
 
     receiver = Receiver()
     sender = Sender(receiver, control_method, windows_size, packet_size)
-    sender.set_ts_interference(probability)
+    sender.set_ts_interference(probability, model, P01, P10)
 
     img_array = np.asarray(img)
     sender.data_to_binary(img_array)
@@ -18,18 +19,23 @@ def start_test(chosen_algorithm, control_method, probability, windows_size, pack
 
     sender.send_frames(chosen_algorithm)
     final_img = receiver.finalize_img()
-    
+
     return receiver.numberOfAcceptedPackets, receiver.numberOfRejectedPackets, sender.ber()
 
-times_to_repeat = 20
+
+times_to_repeat = 1
 window_size = 4
 packet_size = 4
 probability = 0.01
-while (window_size <= 16):
-    while (packet_size <= 16):
-        while (probability <= 0.5):
+P01 = 0.01
+P10 = 0.01
+while (window_size <= 4):#16
+    while (packet_size <= 4):#16
+        while (probability <= 0.1):#0.5
             for alg in range(3):
                 for control_method in range(3):
+                    model = 0
+                    file.write(f'Model: {model} \n')
                     file.write(f'Window size: {window_size} \n')
                     file.write(f'Packet size: {packet_size} \n')
                     file.write(f'Probability: {probability} \n')
@@ -41,8 +47,39 @@ while (window_size <= 16):
                     sum_ber = 0
 
                     for count in range(times_to_repeat):
-                        accepted, rejected, ber = start_test(alg, control_method, probability, window_size, packet_size)
-                        
+                        accepted, rejected, ber = start_test(alg, control_method, probability, window_size, packet_size,
+                                                             model,0,0)
+
+                        if (count == 0):
+                            sum_accepted = accepted
+                        sum_rejected += rejected
+                        sum_ber += ber
+
+                    sum_rejected = sum_rejected / times_to_repeat
+                    sum_ber = sum_ber / times_to_repeat
+
+                    file.write(f'Accepted: {sum_accepted} \n')
+                    file.write(f'Rejected: {sum_rejected} \n')
+                    file.write(f'Ber: {sum_ber} \n\n')
+
+                    model = 1
+                    file.write(f'Model: {model} \n')
+                    file.write(f'Window size: {window_size} \n')
+                    file.write(f'Packet size: {packet_size} \n')
+                    file.write(f'Probability: {probability} \n')
+                    file.write(f'P01: {P01} \n')
+                    file.write(f'P10: {P10} \n')
+                    file.write(f'Alg: {alg} \n')
+                    file.write(f'Control method: {control_method} \n')
+
+                    sum_accepted = 0
+                    sum_rejected = 0
+                    sum_ber = 0
+
+                    for count in range(times_to_repeat):
+                        accepted, rejected, ber = start_test(alg, control_method, probability, window_size, packet_size,
+                                                             model, P01, P10)
+
                         if (count == 0):
                             sum_accepted = accepted
                         sum_rejected += rejected
@@ -55,11 +92,15 @@ while (window_size <= 16):
                     file.write(f'Rejected: {sum_rejected} \n')
                     file.write(f'Ber: {sum_ber} \n\n')
             probability *= 2
+            P01 *= 2
+            P10 *= 2
 
         packet_size *= 2
-        
+
     packet_size = 4
     probability = 0.01
+    P01 = 0.01
+    P10 = 0.01
     window_size *= 2
 
 file.close()
