3b5359b444573d1609efa3a137d550ad383cddcc
unknown <adrian.malecki99@o2.pl>
2020-06-10 03:28:11 +0200

    Update

--- Test.py
+++ Test.py
@@ -23,15 +23,15 @@ def start_test(chosen_algorithm, control_method, probability, windows_size, pack
     return receiver.numberOfAcceptedPackets, receiver.numberOfRejectedPackets, sender.ber()
 
 
-times_to_repeat = 1
+times_to_repeat = 10
 window_size = 4
 packet_size = 4
 probability = 0.01
 P01 = 0.01
 P10 = 0.01
-while (window_size <= 4):#16
-    while (packet_size <= 4):#16
-        while (probability <= 0.1):#0.5
+while (window_size <= 16):
+    while (packet_size <= 16):
+        while (probability <= 0.5):
             for alg in range(3):
                 for control_method in range(3):
                     model = 0
@@ -63,36 +63,38 @@ while (window_size <= 4):#16
                     file.write(f'Ber: {sum_ber} \n\n')
 
                     model = 1
-                    file.write(f'Model: {model} \n')
-                    file.write(f'Window size: {window_size} \n')
-                    file.write(f'Packet size: {packet_size} \n')
-                    file.write(f'Probability: {probability} \n')
-                    file.write(f'P01: {P01} \n')
-                    file.write(f'P10: {P10} \n')
-                    file.write(f'Alg: {alg} \n')
-                    file.write(f'Control method: {control_method} \n')
+                    while (P01 <= 0.5):
+                        file.write(f'Model: {model} \n')
+                        file.write(f'Window size: {window_size} \n')
+                        file.write(f'Packet size: {packet_size} \n')
+                        file.write(f'Probability: {probability} \n')
+                        file.write(f'P01: {P01} \n')
+                        file.write(f'P10: {P10} \n')
+                        file.write(f'Alg: {alg} \n')
+                        file.write(f'Control method: {control_method} \n')
 
-                    sum_accepted = 0
-                    sum_rejected = 0
-                    sum_ber = 0
+                        sum_accepted = 0
+                        sum_rejected = 0
+                        sum_ber = 0
 
-                    for count in range(times_to_repeat):
-                        accepted, rejected, ber = start_test(alg, control_method, probability, window_size, packet_size,
-                                                             model, P01, P10)
+                        for count in range(times_to_repeat):
+                            accepted, rejected, ber = start_test(alg, control_method, probability, window_size, packet_size,
+                                                                 model, P01, P10)
 
-                        if (count == 0):
-                            sum_accepted = accepted
-                        sum_rejected += rejected
-                        sum_ber += ber
+                            if (count == 0):
+                                sum_accepted = accepted
+                            sum_rejected += rejected
+                            sum_ber += ber
 
-                    sum_rejected = sum_rejected / times_to_repeat
-                    sum_ber = sum_ber / times_to_repeat
+                        sum_rejected = sum_rejected / times_to_repeat
+                        sum_ber = sum_ber / times_to_repeat
 
-                    file.write(f'Accepted: {sum_accepted} \n')
-                    file.write(f'Rejected: {sum_rejected} \n')
-                    file.write(f'Ber: {sum_ber} \n\n')
+                        file.write(f'Accepted: {sum_accepted} \n')
+                        file.write(f'Rejected: {sum_rejected} \n')
+                        file.write(f'Ber: {sum_ber} \n\n')
+                        P01 *= 2
             probability *= 2
-            P01 *= 2
+            P01 = 0.01
             P10 *= 2
 
         packet_size *= 2
