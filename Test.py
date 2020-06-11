30897efe0359ba16ddaeb20760d6468d891f639b
Daniil Rylski <addanik@gmail.com>
2020-06-11 17:55:04 +0300

    Make test a function

--- Test.py
+++ Test.py
@@ -3,8 +3,6 @@ from PIL import Image
 from Sender import Sender
 from Receiver import Receiver
 
-file = open("test_results.txt", "w")
-
 
 def start_test(chosen_algorithm, control_method, probability, windows_size, packet_size, model, P01, P10):
     img = Image.open("test.jpg")
@@ -18,19 +16,30 @@ def start_test(chosen_algorithm, control_method, probability, windows_size, pack
     sender.split_array()
 
     sender.send_frames(chosen_algorithm)
+
+    error_counter = 0
+    ber = 0
+    for j in range(0, len(receiver.result)):
+        final_pack = receiver.result[j].astype(np.uint8)
+        start_pack = sender.packets[j]
+        for i in range(0, packet_size):
+            if start_pack[i] != final_pack[i]:
+                error_counter += 1
+        j += 1
+    ber = error_counter/(len(receiver.result)*packet_size)*100
+
     final_img = receiver.finalize_img()
+    return receiver.numberOfAcceptedPackets, receiver.numberOfRejectedPackets, ber
 
-    return receiver.numberOfAcceptedPackets, receiver.numberOfRejectedPackets, sender.ber()
-
-
-times_to_repeat = 10
-window_size = 4
-packet_size = 4
-probability = 0.01
-P01 = 0.01
-P10 = 0.01
-while (window_size <= 16):
-    while (packet_size <= 16):
+def run_test():
+    file = open("test_results.txt", "w")
+    times_to_repeat = 10
+    window_size = 4
+    packet_size = window_size
+    probability = 0.01
+    P01 = 0.01
+    P10 = 0.01
+    while (window_size <= 16):
         while (probability <= 0.5):
             for alg in range(3):
                 for control_method in range(3):
@@ -48,7 +57,7 @@ while (window_size <= 16):
 
                     for count in range(times_to_repeat):
                         accepted, rejected, ber = start_test(alg, control_method, probability, window_size, packet_size,
-                                                             model,0,0)
+                                                                model,0,0)
 
                         if (count == 0):
                             sum_accepted = accepted
@@ -79,7 +88,7 @@ while (window_size <= 16):
 
                         for count in range(times_to_repeat):
                             accepted, rejected, ber = start_test(alg, control_method, probability, window_size, packet_size,
-                                                                 model, P01, P10)
+                                                                    model, P01, P10)
 
                             if (count == 0):
                                 sum_accepted = accepted
@@ -93,16 +102,16 @@ while (window_size <= 16):
                         file.write(f'Rejected: {sum_rejected} \n')
                         file.write(f'Ber: {sum_ber} \n\n')
                         P01 *= 2
-            probability *= 2
-            P01 = 0.01
-            P10 *= 2
+                probability *= 2
+                P01 = 0.01
+                P10 *= 2
 
-        packet_size *= 2
+            packet_size *= 2
 
-    packet_size = 4
-    probability = 0.01
-    P01 = 0.01
-    P10 = 0.01
-    window_size *= 2
+        packet_size = 4
+        probability = 0.01
+        P01 = 0.01
+        P10 = 0.01
+        window_size *= 2
 
-file.close()
+    file.close()
